a0bdf25ddf540133a3543fa82da418677674413a
ukaegbu victorygold <you@example.com>
2025-02-01 20:45:19 +0100

    Horizon

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,36 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+const loggedIn =  {firstName: 'Victory' ,lastName: 'Ukaegbu', email: 'gvictory3434@gmail.com'}
+
   return (
-    <div>Home</div>
-  )
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently."
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={2543.67}
+          />
+        </header>
+        RECENT TRANSACTION
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 280.56 }, { currentBalance: 569.58}]}
+      />
+    </section>
+  );
 }
 
 export default Home
